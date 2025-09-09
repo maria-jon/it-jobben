e81d864011d4bf514e5f49dfaab8e8a08e87eba1
maria-jon <mariajonasson93@gmail.com>
2025-09-09 14:54:36 +0200

    :sparkles: update type Job

--- src/models/Job.ts
+++ src/models/Job.ts
@@ -1,4 +1,14 @@
 export type Job = {
     id: string;
     headline: string;
+    description: string;
+    application_deadline: string;
+    employer: {
+        name: string;
+        workplace: string;
+    }
+    occupation: {
+        label: string;
+    }
+    publication_date: string;
 };
