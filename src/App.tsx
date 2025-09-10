1408b17ebd30ab596356c83362943aa532ccb8cd
AgnesWilson <agnes.wilson97@hotmail.com>
2025-09-10 16:39:26 +0200

    🔥 Remove old unnecessary code from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { getJobById, getJobs } from "./services/jobService";
-
-// Hard coded id and buttons to fetch all job ads 
-// and single job ad can be removed whren the routing is in place 
-
-const id = "29997240"
-
 function App() {
   return <>
-  <button onClick={getJobs}>Alla jobb</button>
-  <button onClick={() => getJobById(id)}>Enskilt jobb</button>
   </>;
 }
 
