8869e188107b785d8cfa441792cca7c31af65596
AgnesWilson <agnes.wilson97@hotmail.com>
2025-09-12 17:34:40 +0200

    🚧 Working on marking an add as a favoite
    Added a button that updates a numer (for now) when you click it.

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,23 +1,48 @@
 import type { Job } from "../models/Job";
 
-import { DigiInfoCardMulti, DigiTypography, DigiTypographyTime } from "@digi/arbetsformedlingen-react";
-import { InfoCardMultiHeadingLevel, InfoCardMultiType, TypographyTimeVariation, TypographyVariation } from "@digi/arbetsformedlingen";
+import { DigiButton, DigiIconHeartSolid, DigiInfoCardMulti, DigiTypography, DigiTypographyTime } from "@digi/arbetsformedlingen-react";
+import { ButtonSize, ButtonVariation, InfoCardMultiHeadingLevel, InfoCardMultiType, TypographyTimeVariation, TypographyVariation } from "@digi/arbetsformedlingen";
+import { useState } from "react";
 
 type JobCardProps = {
     job: Job;
 }
 
 export const JobCard = ({ job }: JobCardProps) => {
+
+    // ta bort sen
+    const [test, setTest] = useState(1);
+
     return(
         <>
+        <h1>{test}</h1>
             <DigiInfoCardMulti
                 afHeading={job.headline}
                 afHeadingLevel={InfoCardMultiHeadingLevel.H2}
                 afType={InfoCardMultiType.ENTRY}
-                afLinkHref={`/ad/${job.id}`}        
+                afLinkHref={`/ad/${job.id}`}
+                style={
+                    {position: "relative",} as React.CSSProperties}       
             >
-                <DigiTypography
-                    afVariation={TypographyVariation.SMALL}
+
+            <DigiButton
+                afSize={ButtonSize.MEDIUM}
+                afVariation={ButtonVariation.PRIMARY}
+                style={
+                    {
+                    position: "absolute",
+                    top: "45px",
+                    right: "80px",
+                    } as React.CSSProperties
+                }
+                afFullWidth={false}
+                onAfOnClick={() => setTest(test + 1)}>
+                Spara som favorit
+            <DigiIconHeartSolid slot="icon-secondary" />
+            </DigiButton>
+
+            <DigiTypography
+                    afVariation={TypographyVariation.SMALL}  
                 >
                     <h3>{job.employer.name}</h3>
                     <p>{job.occupation.label}</p>
