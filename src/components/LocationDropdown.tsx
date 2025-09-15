c4fa54c22386d0428135bd589041d001c6be2199
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-09-15 13:52:33 +0200

    :sparkles: Add DigiContextMenu with static menu items

--- src/components/LocationDropdown.tsx
+++ src/components/LocationDropdown.tsx
@@ -1,4 +1,19 @@
+import { DigiContextMenu } from "@digi/arbetsformedlingen-react";
+
 export const LocationDropdown = () => {
-  return;
-  <div>LocationDropdown</div>;
+  const menuItems = [
+    { id: 0, title: "Alla orter" },
+    { id: 1, title: "Distans (remote)" },
+    { id: 2, title: "Hybrid" },
+  ];
+
+  return (
+    <DigiContextMenu
+      afTitle="Välj ort"
+      afMenuItems={menuItems}
+      onAfChangeItem={() => {}}
+    ></DigiContextMenu>
+  );
 };
+
+<div>LocationDropdown</div>;
